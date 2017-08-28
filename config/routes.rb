73e8a27e2f6793d88564236b073c333a169f3f2e
jamesrsilvester <jamesrsilvester@gmail.com>
2017-08-28 11:04:50 -0700

    "dan suggested I commit"

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
+  # Library Routes
+  get '/libraries', to: 'libraries#index'
+  get '/libraries/new', to:'libraries#new', as: 'new_library'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # User Routes
   root to: 'users#index'
-
   get '/users/new', to: 'users#new', as: 'new_user'
-
   post '/users', to: 'users#create'
-
   get '/users/:id', to: 'users#show', as: 'user'
-
   get '/login', to: 'sessions#new'
-
   get '/logout', to: 'sessions#destroy'
-
   post '/sessions', to: 'sessions#create'
 
 end
